a191edd15b7db88b2ffc494cf2a79e82484ba6dc
HectorPabloMonasterio <angeljavier1885@gmail.com>
2023-05-11 17:34:38 -0300

    Agregué la carpeta backend

---
+++ backend/src/main/java/com/portfolio/pablomonasterio/PablomonasterioApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.pablomonasterio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PablomonasterioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PablomonasterioApplication.class, args);
+	}
+
+}
